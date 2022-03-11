9a434ef8b6f4ffa940e36ff56af50bcdce6d9c0c
Aniruddha <aniruddha1712@gmail.com>
2022-03-11 16:14:57 +0530

    [Aniruddha] Refactored: implemented calculation of daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,11 +3,15 @@
 echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1
+empRatePerHr=20
 randomCheck=$((RANDOM%2))
 
 if [ $isPresent -eq $randomCheck ]
 then
 	echo "Employee is Present"
+	empHrs=8
+	salary=$(($empHrs * $empRatePerHr))
 else
 	echo "Employee is Absent"
+	salary=0
 fi
